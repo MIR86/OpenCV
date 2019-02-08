bac0e6314224bc9428a557adfde397fea533d216
MIR86 <vladimir.ebox@gmail.com>
2019-02-08 14:36:51 +0100

    Add files via upload
    RUN: MakeImages.jave (main func.)
    
    Problemmes occurantes:
    - windows 10 problèmmes de librairies (librairies ne ce retrouve pas)
     Il faut télécharger les librairies OpenCV et Bytedecode et creer dans les environnent de variable (chemin / path) et dans le IDE ajouter les librairies externes.

---
+++ Smoother.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_javacv;
+
+/**
+ *
+ * @author Vincent-ROG
+ */
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+
+public class Smoother {
+    public static void smooth(String filename) { 
+        IplImage image = cvLoadImage(filename);
+        if (image != null) {
+            cvSmooth(image, image);
+            cvSaveImage(filename, image);
+            cvReleaseImage(image);
+        }
+    }
+}
